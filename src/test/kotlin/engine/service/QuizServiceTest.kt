62f912309e554947cfb9632b775d2f425f0532f0
Torgrim Limstrand <torgrimlimstrand@hotmail.com>
2025-06-22 20:50:53 +0200

    Add unit tests for `checkAnswer` method in `QuizServiceTest`

--- src/test/kotlin/engine/service/QuizServiceTest.kt
+++ src/test/kotlin/engine/service/QuizServiceTest.kt
@@ -1,6 +1,7 @@
 package engine.service
 
 import engine.dto.QuizQuestionResponse
+import engine.dto.QuizResponse
 import io.mockk.impl.annotations.MockK
 import io.mockk.impl.annotations.OverrideMockKs
 import io.mockk.junit5.MockKExtension
@@ -32,11 +33,24 @@ class QuizServiceTest {
                 "What is depicted on the Java logo?",
                 listOf("Robot", "Tea leaf", "Cup of coffee", "Bug"))
 
+        fun checkAnswerCorrectAnswer(): QuizResponse = QuizResponse(true, "Congratulations, you're right!")
+        fun checkAnswerWrongAnswer(): QuizResponse = QuizResponse(false, "Wrong answer! Please, try again.")
     }
 
     @Test
     fun `getSampleQuiz should return the hardcoded sample response`() {
         val result = quizService.getSampleQuiz()
         assertEquals(expectedSampleQuiz(), result)
+    }
+
+    @Test
+    fun `checkAnswer should response with correct answer`() {
+        val result = quizService.checkAnswer(2)
+        assertEquals(checkAnswerCorrectAnswer(), result)
+    }
+    @Test
+    fun `checkAnswer response with wrong answer`() {
+        val result = quizService.checkAnswer(1)
+        assertEquals(checkAnswerWrongAnswer(), result)
     }
 }
