8abe123283f776f8cb0fcdaf34b4c9d0ce720087
Torgrim Limstrand <torgrimlimstrand@hotmail.com>
2025-06-23 20:33:17 +0200

    Add tests for `addQuiz` to verify unauthorized access handling and quiz saving logic

--- src/test/kotlin/engine/service/QuizServiceTest.kt
+++ src/test/kotlin/engine/service/QuizServiceTest.kt
@@ -2,6 +2,7 @@ package engine.service
 
 import engine.dto.QuizQuestionResponse
 import engine.dto.QuizResponse
+import engine.entity.Quiz
 import io.mockk.impl.annotations.MockK
 import io.mockk.impl.annotations.OverrideMockKs
 import io.mockk.junit5.MockKExtension
@@ -12,8 +13,15 @@ import engine.repository.QuizCompletionsRepository
 import engine.service.QuizTestData.checkAnswerCorrectAnswer
 import engine.service.QuizTestData.expectedSampleQuiz
 import io.mockk.every
+import io.mockk.slot
+import io.mockk.verify
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertSame
+import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.springframework.http.HttpStatus
+import org.springframework.web.server.ResponseStatusException
 
 @ExtendWith(MockKExtension::class)
 class QuizServiceTest {
@@ -48,6 +56,38 @@ class QuizServiceTest {
         val expected = QuizTestData.checkAnswerWrongAnswer()
         val result = quizService.checkAnswer(1)
         assertEquals(expected, result)
+    }
+
+    @Test
+    fun `addQuiz should return unauhorized if user not found`() {
+        every { userRepository.findUserByEmail(any()) } returns null
+        val ex = assertThrows(ResponseStatusException::class.java) {
+            quizService.addQuiz(QuizTestData.postQuizRequest(), "123@hotmail.com")
+        }
+        assertEquals(HttpStatus.UNAUTHORIZED, ex.statusCode)
+        verify(exactly = 0) { quizRepository.save(any()) }
+    }
+
+    @Test
+    fun `addQuiz should save a correctly requested quiz`() {
+        val post = QuizTestData.postQuizRequest()
+        val email = "123@hotmail.com"
+        val user = QuizTestData.samleUser()
+
+
+        every { userRepository.findUserByEmail(any()) } returns user
+        every { quizRepository.save(any()) } answers { firstArg<Quiz>() }
+        val result = quizService.addQuiz(post, email)
+        verify(exactly = 1) { userRepository.findUserByEmail(email) }
+
+        val slot = slot<Quiz>()
+        verify(exactly = 1){quizRepository.save(capture(slot))}
+        assertEquals(post.title,   slot.captured.title)
+        assertEquals(post.text,    slot.captured.text)
+        assertEquals(post.options, slot.captured.options)
+        assertEquals(post.answer,  slot.captured.answer)
+        assertSame(user,           slot.captured.user)
+        assertSame(slot.captured, result)
     }
 
 }
--- src/test/kotlin/engine/service/QuizTestData.kt
+++ src/test/kotlin/engine/service/QuizTestData.kt
@@ -1,9 +1,12 @@
 package engine.service
 
+import engine.dto.PostQuizRequest
 import engine.dto.QuizQuestionResponse
 import engine.dto.QuizResponse
+import engine.entity.Quiz
+import engine.entity.User
 
- object QuizTestData {
+object QuizTestData {
     fun expectedSampleQuiz(): QuizQuestionResponse = QuizQuestionResponse(
             "The Java Logo",
             "What is depicted on the Java logo?",
@@ -11,4 +14,17 @@ import engine.dto.QuizResponse
 
     fun checkAnswerCorrectAnswer(): QuizResponse = QuizResponse(true, "Congratulations, you're right!")
     fun checkAnswerWrongAnswer(): QuizResponse = QuizResponse(false, "Wrong answer! Please, try again.")
+    fun postQuizRequest(): PostQuizRequest = PostQuizRequest(
+            title = "The Java Logo",
+            text = "What is depicted on the Java logo?",
+            options = listOf("Robot", "Tea leaf", "Cup of coffee", "Bug"),
+            answer = listOf(2))
+
+
+
+    fun samleUser(): User = User(
+            null,
+            "123@hotmail.com",
+            "123",
+            )
 }
